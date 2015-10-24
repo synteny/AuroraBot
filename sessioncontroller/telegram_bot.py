965bfc34f6f24359cd9ef3f71d1a3eb18329b49a
Alexey Grunichev <grunichev@gmail.com>
2015-10-24 18:05:24 +0300

    make telegram_bot importable

--- sessioncontroller/telegram_bot.py
+++ sessioncontroller/telegram_bot.py
@@ -31,30 +31,35 @@ def reply(update, message):
         text=message,
     )
 
-offset = 0
-print 'Press Ctrl+C to kill...'
-while True:
-    updates = bot.getUpdates(offset=offset+1)
-    for update in updates:
-        text = update.message.text
-        text_split = text.split(' ', 1)
-        command, arguments = text_split[0], None
-        if len(text_split) > 1:
-            arguments = text_split[1]
 
-        if command == '/subscribe':
-            geo = geo_code(arguments)
-            if geo is None:
-                reply(update, "Не удалось определить координаты места")
+def main():
+    offset = 0
+    print 'Press Ctrl+C to kill...'
+    while True:
+        updates = bot.getUpdates(offset=offset+1)
+        for update in updates:
+            text = update.message.text
+            text_split = text.split(' ', 1)
+            command, arguments = text_split[0], None
+            if len(text_split) > 1:
+                arguments = text_split[1]
+
+            if command == '/subscribe':
+                geo = geo_code(arguments)
+                if geo is None:
+                    reply(update, "Не удалось определить координаты места")
+                else:
+                    geo_id = lat_lon_to_cell(geo[0], geo[1])
+                    subscribe(update.message.chat_id, arguments, geo_id)
+                    reply(update, "Вы подписаны и получите уведомление о сиянии в Ваше регионе как только так сразу!")
+
+            elif command == '/unsubscribe':
+                unsubscribe(update.message.chat_id)
+                reply(update, "Вы успешно отписаны.")
+
             else:
-                geo_id = lat_lon_to_cell(geo[0], geo[1])
-                subscribe(update.message.chat_id, arguments, geo_id)
-                reply(update, "Вы подписаны и получите уведомление о сиянии в Ваше регионе как только так сразу!")
+                reply(update, "Неизвестная комманда")
+            offset = update.update_id
 
-        elif command == '/unsubscribe':
-            unsubscribe(update.message.chat_id)
-            reply(update, "Вы успешно отписаны.")
-
-        else:
-            reply(update, "Неизвестная комманда")
-        offset = update.update_id
+if __name__ == '__main__':
+    main()
