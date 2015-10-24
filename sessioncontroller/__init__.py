1a8d3696941cbfba95a46979e356321f7111709f
Vladimir Smirnov <vladi.smirnov@gmail.com>
2015-10-24 16:49:53 +0300

    lat-lon conversion to cell id

---
+++ sessioncontroller/__init__.py
@@ -0,0 +1,6 @@
+LON_BLOCKS = 1096
+LAT_BLOCKS = 512
+
+# converts (lat, lon) to linear cell index, based on a 1096 x 512 grid
+def lat_lon_to_cell(lat, lon):
+    return int((90.0 - lat) * LAT_BLOCKS / 180.0) * (LON_BLOCKS - 1) + int((lon + 180.0) * LAT_BLOCKS / 360.0)
---
+++ sessioncontroller/test__init__.py
@@ -0,0 +1,12 @@
+from sessioncontroller import *
+
+import unittest
+
+class TestGlobalMethods(unittest.TestCase):
+    def test_lat_lon_to_cell(self):
+        self.assertEqual(lat_lon_to_cell(90.0, -180.0), 0)
+        self.assertEqual(lat_lon_to_cell(0, 0), LAT_BLOCKS * LON_BLOCKS / 2)
+        self.assertEqual(lat_lon_to_cell(-90, 180.0), LAT_BLOCKS * LON_BLOCKS)
+
+if __name__ == '__main__':
+    unittest.main()
